6de403bb9282f551c65f3d0423e7d774def6862d
Hamlet Ghochikyan <ghochikyanhamlet8@gmail.com>
2025-11-17 02:25:19 +0400

    Add project1 folder

---
+++ project1/src/Counter.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+export default function Counter() {
+    const [count, setCount] = useState(0);
+    return(
+        <div>
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count => count + 1)}>+</button>
+            <button 
+                onClick={() => setCount(count => count - 1)}
+                disabled = {count === 0}>-</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+        </div>
+    );
+}
